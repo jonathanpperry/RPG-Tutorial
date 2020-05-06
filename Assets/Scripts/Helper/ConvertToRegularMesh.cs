02108cad83c21ce18b3d4ce58cadd9da30b3d1a7
Jonathan Perry <jpper@umich.edu>
2020-05-06 22:15:29 +0900

    Finally reset player graphic y position and player stands on top of grass in and out of game mode

---
+++ Assets/Scripts/Helper/ConvertToRegularMesh.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConvertToRegularMesh : MonoBehaviour
+{
+    [ContextMenu("Convert to regular mesh")]
+    void Convert()
+    {
+        SkinnedMeshRenderer skinnedMeshRenderer = GetComponent<SkinnedMeshRenderer>();
+        MeshRenderer meshRenderer = gameObject.GetComponent<MeshRenderer>();
+        MeshFilter meshFilter = gameObject.GetComponent<MeshFilter>();
+
+        meshFilter.sharedMesh = skinnedMeshRenderer.sharedMesh;
+        meshRenderer.sharedMaterials = skinnedMeshRenderer.sharedMaterials;
+
+        DestroyImmediate(skinnedMeshRenderer);
+        DestroyImmediate(this);
+    }
+}
